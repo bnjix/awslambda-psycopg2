e7403ea43daa6d10bdba12b71e10d77701412688
Benjamin Peyrot <benji.peyrot@gmail.Com>
2018-01-19 14:56:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='psycopg2',    # This is the name of your PyPI-package.
     version='0.1',             # Update the version number for new releases
-    packages=find_packages()
+    packages=find_packages(),
+    include_package_data=True
 )
