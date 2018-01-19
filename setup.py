cc405e999c5ea852e9ac5667a01788678bda2429
Benjamin Peyrot <benji.peyrot@gmail.Com>
2018-01-19 14:35:36 +0100

    test setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='lambda-psycopg2',    # This is the name of your PyPI-package.
+    version='0.1',             # Update the version number for new releases
+    packages=find_packages()
+)
