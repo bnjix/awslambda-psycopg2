41a27a301be796a82823c8f4bfbac8b0288d8ea7
Benjamin Peyrot <benji.peyrot@gmail.Com>
2018-01-19 14:37:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='lambda-psycopg2',    # This is the name of your PyPI-package.
+    name='psycopg2',    # This is the name of your PyPI-package.
     version='0.1',             # Update the version number for new releases
     packages=find_packages()
 )
